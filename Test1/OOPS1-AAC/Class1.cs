21fce333c55a50d6932016360609dfe62b442c78
Muhammad Jafar <mjafar@jcp.com>
2018-07-20 10:11:56 +0530

    oops association abstraction composition

---
+++ Test1/OOPS1-AAC/Class1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS1_AAC
+{
+    public class Class1
+    {
+    }
+    /// <summary>
+    /// An association is a "using" relationship between two or more objects in which the objects
+    /// have their own life time and there is no owner.
+    /// As an example, imagine the relationship between a doctor and a patient.
+    /// A doctor can be associated with multiple patients and at the same time, 
+    /// one patient can visit multiple doctors for treatment and/or consultation.
+    /// Each of these objects have their own life-cycle and there is no owner. 
+    /// In other words, the objects that are part of the association relationship can be created and destroyed independently.
+    /// </summary>
+    class Doctor
+    {
+        public Patient[] patients { get; set; }
+    }
+
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    /// <summary>
+    /// Aggregation is a special type of Association. Aggregation is "*the*" or has a relationship among objects
+    /// Aggregation is a specialized form of association between two or more objects in which the objects have
+    /// their own life-cycle but there exists an ownership as well.
+    /// As an example, an employee may belong to multiple departments in the organization. 
+    /// However, if the department is deleted, the employee object wouldn't be destroyed.
+    /// </summary>
+    class Address
+    {
+        string _city, _zip;
+        public Address(string city , string zip)
+        {
+            _city = city;
+            _zip = zip;
+        }
+    }
+    class Employee
+    {
+        private dynamic _address;
+        string _name;
+        int _id;
+
+        public Employee(string name, int id, Address address)
+        {
+            _name = name;_id = id;_address = address;
+        }
+    }
+    class TestAggregation
+    {
+        //private readonly Address address;
+        public TestAggregation()
+        {
+            Address address = new Address("Bangalore", "123456");
+            Employee employee = new Employee("Jafar", 12, address);
+        }
+    }
+
+    /// <summary>
+    /// Composition is a specialized form of aggregation in which if the parent object is destroyed, 
+    /// the child objects would cease to exist. It is actually a strong type of aggregation and is also referred to
+    /// as a "death" relationship. 
+    /// As an example, a house is composed of one or more rooms. If the house is destroyed, 
+    /// all the rooms that are part of the house are also destroyed as they cannot exist by themselves.
+    ///  If we delete the parent object then the child object(s) will also be deleted
+    /// </summary>
+    class Composition
+    {
+
+    }
+    class House
+    {
+        private Room room;
+        public House()
+        {
+            room = new Room();
+        }
+    }
+    class Room
+    {
+
+    }
+}
---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<string, int> = new Dictionary<string, int>();
+            //1. reverse  sentence  // hey you Bub = Bub! you, Hey
+            Reverse("Hello Jafar Hru?");
+
+            var s = MonoalphabeticEncryption("the", 5);
+            var ss = ToLong("three");
+            Sample sample = new Sample();
+            sample.Print();
+           
+
+            ISample sample1 = new Sample();
+            sample1.Print();
+
+        }
+
+        private static void Reverse(string v)
+        {
+            string[] a = v.Split(' ');
+            string ss = string.Empty;
+            Array.Reverse(a);
+            Console.WriteLine("Reverse String is:");
+            for (int i = 0; i <= a.Length - 1; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                ss = a[i] + "" + ' ';
+            }
+        }
+    
+
+        private static Dictionary<string, long> numberTable =
+    new Dictionary<string, long>
+        {{"zero",0},{"one",1},{"two",2},{"three",3},{"four",4},
+        {"five",5},{"six",6},{"seven",7},{"eight",8},{"nine",9},
+        {"ten",10},{"eleven",11},{"twelve",12},{"thirteen",13},
+        {"fourteen",14},{"fifteen",15},{"sixteen",16},
+        {"seventeen",17},{"eighteen",18},{"nineteen",19},{"twenty",20},
+        {"thirty",30},{"forty",40},{"fifty",50},{"sixty",60},
+        {"seventy",70},{"eighty",80},{"ninety",90},{"hundred",100},
+        {"thousand",1000},{"million",1000000},{"billion",1000000000},
+        {"trillion",1000000000000},{"quadrillion",1000000000000000},
+        {"quintillion",1000000000000000000}};
+        public static long ToLong(string numberString)
+        {
+            var numbers = Regex.Matches(numberString, @"\w+").Cast<Match>()
+                 .Select(m => m.Value.ToLowerInvariant())
+                 .Where(v => numberTable.ContainsKey(v))
+                 .Select(v => numberTable[v]);
+            long acc = 0, total = 0L;
+            foreach (var n in numbers)
+            {
+                if (n >= 1000)
+                {
+                    total += (acc * n);
+                    acc = 0;
+                }
+                else if (n >= 100)
+                {
+                    acc *= n;
+                }
+                else acc += n;
+            }
+            return (total + acc) * (numberString.StartsWith("minus",
+                  StringComparison.InvariantCultureIgnoreCase) ? -1 : 1);
+        }
+        private static string MonoalphabeticEncryption(string sPlainText, int key)
+        {
+            sPlainText = sPlainText.ToUpper();
+            string[] cipherTextChars = new string[sPlainText.Length];
+            var alphabet = Enumerable.Range('A', 26).Select(x => (char)x).ToList();
+
+            for (int i = 0; i < sPlainText.Length; i++)
+            {  
+                    char value = sPlainText[i];
+                    int index = alphabet.FindIndex(a => a.Equals(value));
+                    cipherTextChars[i] = alphabet[index + key].ToString();
+            }
+
+            return string.Join("",cipherTextChars).ToString();
+
+        }
+    }
+    interface ISample
+    {
+        void Print(string a = "Jafar");
+    }
+    class Sample : ISample
+    {
+        public void Print(string a = "Sample")
+        {
+            Console.WriteLine(a);//throw new NotImplementedException();
+        }
+    }
+
+
+}
